12de55cd4b4fd366a01396d36be7f4bb6f734699
avik <avikez@gmail.com>
2013-11-27 19:27:29 +0400

    Revert "Add bonuses to menu"
    This reverts commit 10225f93c00e29fd4ec9a3a57228dca0445f577c.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,9 +63,6 @@ $(function() {
     $('.popover_hover').popover(
       { trigger: "hover" }
     );
-	$('.popover_bonuses').popover({
-      html: true;
-	});
     $('.tooltip-hover').tooltip(
     );
     $('#info_tab a').click(function (e) {
