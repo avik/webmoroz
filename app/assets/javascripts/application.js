e7bfc668eafee990125eeb0aac9f6e5a51e8a726
avik <avikez@gmail.com>
2013-12-05 01:00:58 +0400

    fix javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,38 +21,34 @@
 //= require dashboard
 //= require_self
 
-$.getScript('http://vk.com/js/api/openapi.js?105');
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1&appId=553281794754505";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '553281794754505', // App ID
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-  });
-  FB.Event.subscribe('edge.create', function(param) {
-    $.getJSON("/users/mark_increase_social", {social: 'fb'}, function( mark ) {
-      if (mark) $("#num_mark").html(mark);
+$(document).ready(function() {
+  $.ajaxSetup({ cache: true });
+  $.getScript('//connect.facebook.net/en_UK/all.js', function(){
+    FB.init({
+      appId      : '553281794754505', // App ID
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+    FB.Event.subscribe('edge.create', function(param) {
+      $.getJSON("/users/mark_increase_social", {social: 'fb'}, function( mark ) {
+        if (mark) $("#num_mark").html(mark);
+      });
     });
   });
-  VK.init({apiId: 3965772, onlyWidgets: true});
-  VK.Widgets.Like("vk_like", {type: "button", height: 24});
-  VK.Widgets.Comments("vk_comments", {limit: 15, width: "738", attach: "*"});
-  VK.Widgets.Group("vk_groups", {mode: 2, wide: 1, width: "718", height: "400"}, 61591348);
-  VK.Observer.subscribe('widgets.like.shared', function(param) {
-    $.getJSON("/users/mark_increase_social", {social: 'vk'}, function( mark ) {
-      if (mark) $("#num_mark").html(mark);
+  $.getScript('http://vk.com/js/api/openapi.js?105', function(){
+    VK.init({apiId: 3965772, onlyWidgets: true});
+    VK.Widgets.Like("vk_like", {type: "button", height: 24});
+    VK.Widgets.Comments("vk_comments", {limit: 15, width: "738", attach: "*"});
+    VK.Widgets.Group("vk_groups", {mode: 2, wide: 1, width: "718", height: "400"}, 61591348);
+    VK.Observer.subscribe('widgets.like.shared', function(param) {
+      $.getJSON("/users/mark_increase_social", {social: 'vk'}, function( mark ) {
+        if (mark) $("#num_mark").html(mark);
+      });
     });
   });
-};
+});
 
 $(function() {
   $(document).ready(function(){
