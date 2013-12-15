43910a5a4b8f76c9b1476586a2b7a6859924e2b1
Dmitry Shmelev <avikez@gmail.com>
2013-12-15 22:39:39 +0400

    fix mailer and git ignore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        UserMailer.welcome_email(current_user.id).deliver
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
