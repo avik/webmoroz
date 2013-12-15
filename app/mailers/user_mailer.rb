7c4bef13321619368bc56b6b59908fd1314fbd86
Dmitry Shmelev <avikez@gmail.com>
2013-12-15 22:03:19 +0400

    fix mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default :from => "\"WebMoroz\" <mail@webmoroz.com>"
-
-  def welcome_email(id)
-    @user = User.find(id)
-    mail(to: @user.email, subject: 'Добро пожаловать, ВебМороз!')
-  end
-end
